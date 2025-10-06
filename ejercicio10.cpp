63310a2006866ebbe4e55d54894c73f8a850b734
andres <andres.ochoa@upb.edu.co>
2025-10-06 17:20:31 -0500

    ejercicios

---
+++ ejercicio10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Los primeros 10 numeros naturales impares son:" << endl;
+
+    for (int i = 1; i <= 19; i += 2) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
